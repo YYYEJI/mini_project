aa4d553d584d304d826f5b45b11fb53c270eab41
Yejisong <22100396@handong.ac.kr>
2022-04-16 14:26:00 +0900

    2 product.h

--- product.h
+++ product.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <Stdlib.h>
+#include <stdlib.h>
 
 typedef struct{
 	char name[30];        // 상품 이름
@@ -12,7 +12,7 @@ typedef struct{
 
 int selectMenu();             
 int addProduct(Product *p);      // 상품 추가
-void inforProduct(Product *p);   // 한 상품 정보 출력
+void infoProduct(Product *p);   // 한 상품 정보 출력
 int updateProduct(Product *p);   // 상품 정보 변경
 void saveData(Product *p);       // 상품의 정보를 file에 저장
 void loadData(Product *p);       // 상품 정보 갯수
